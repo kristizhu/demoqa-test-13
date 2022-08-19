5ab47b7403a1d014b9548b9a27bced93e0f82820
misskriiss <krizhukfu@gmail.com>
2022-08-19 13:11:47 +0300

    locator formatted

--- src/test/java/pages/components/CalenderComponent.java
+++ src/test/java/pages/components/CalenderComponent.java
@@ -3,10 +3,10 @@ package pages.components;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalenderComponent {
-    public void setDate(String day, String month, String year)
-    {
+
+        public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--0" + day).click();
+        $(".react-datepicker__day--" + String.format("%3s", day).replace(' ', '0') + ":not(react-datepicker__day--outside-month").click();
     }
 }
